8f44f9b1b414b7de0bab226ca91ce7e727d8adbb
NatashaChuza <natashachuza@gmail.com>
2020-08-19 02:32:45 +0200

    changed heroku scripts

--- Server.js
+++ Server.js
@@ -5,11 +5,12 @@ const userRoutes = require('./routes/api/User')
 const taskRoutes = require('./routes/api/Task')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const path = require('path')
 
 const app = express()
 
-const port = 5000
-const db = process.env.DB
+const port =process.env.PORT || 5000
+const db = process.env.MONGODB_URI || process.env.DB
 
 mongoose.connect( db, {
     useNewUrlParser: true,
@@ -40,6 +41,16 @@ app.use((err, req, res, next) => {
     console.log(err);
     next();
   })
+
+  //serve static files if in production
+  if(process.env.NODE_ENV === 'production'){
+      //set static folder
+      app.use(express.static('client/build'))
+
+      app.get('*', ( req, res ) =>{
+          res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      })
+  }
 
 app.listen( port, ()=>{
     console.log(`Server is lisstening on port ${port}`)
